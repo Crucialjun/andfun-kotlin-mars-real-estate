5f501b3c391c0dcf6d6717777975fe381990d2dc
Crucial Jun <nich.otieno@gmail.com>
2020-03-10 15:36:20 +0300

    Glide PlaceHolder

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -21,12 +21,18 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
 fun bindmage(imgView: ImageView, imgUrl: String) {
     imgUrl.let {
         val imgUri = it.toUri().buildUpon().scheme("http").build()
-        Glide.with(imgView.context).load(imgUri).into(imgView)
+        Glide.with(imgView.context)
+                .load(imgUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation))
+                .error(R.drawable.ic_broken_image)
+                .into(imgView)
     }
 }
 
